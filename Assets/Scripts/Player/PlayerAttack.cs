034e3a2db4e9767ea69672314906638ccf6cfc67
Michix985 <groth06michal@gmail.com>
2025-12-07 17:49:19 +0100

    added attackSpeed and implemented new input system

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -3,31 +3,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [Header("stats")]
+    public float attackRange = 1.5f;
+    public int damageMelee;
+    public float attackSpeed = 1.0f;
 
     [SerializeField] private Transform attackTransform;
-    [SerializeField] private float attackRange = 1.5f;
     [SerializeField] private LayerMask attackLayer;
     private RaycastHit2D[] hits;
-
-    public int damageMelee;
-    private InputAction attack;
-    
+    private float nextAttackTime;
+    private bool isHoldingAttackButton = false;
     /*
      * v Weapons to add v
      * public int damageLongShot, damageBuuzSaw;
      * v Weapon cycle v
      * public int currentWeapon;
      */
-    private void Start()
-    {
-        attack = new InputAction("Attack", InputActionType.Button, "<Mouse>/leftButton");
-        attack.Enable();
-    }
     void Update()
     {
-        if(Input.GetButtonDown("Attack")) //michix bby girl fix it pls
+        if(isHoldingAttackButton && Time.time > nextAttackTime)
         {
             AttackMeleeBasic();
+            nextAttackTime = Time.time + attackSpeed;
         }
     }
     private void AttackMeleeBasic()
@@ -35,17 +32,24 @@ public class PlayerAttack : MonoBehaviour
         hits = Physics2D.CircleCastAll(attackTransform.position, attackRange, transform.right, 0f, attackLayer);
         for (int i = 0; i < hits.Length; i++)
         {
-            Enemy enemy = hits[i].collider.gameObject.GetComponent<Enemy>();
-
-            if (enemy != null )
+            if (hits[i].collider.gameObject.TryGetComponent<Enemy>(out var enemy))
             {
                 enemy.TakeDamage(damageMelee);
-
             }
         }
     }
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(attackTransform.position, attackRange);
+    }
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            isHoldingAttackButton = true;
+        } else if(context.canceled)
+        {
+            isHoldingAttackButton = false;
+        }
     }
 }

6c4ecdec404a95d144973d88a281e0d987af397a
Michix985 <groth06michal@gmail.com>
2025-12-08 16:40:25 +0100

    TwoState works way better

--- Assets/Scripts/Enemies/TwoStateAI.cs
+++ Assets/Scripts/Enemies/TwoStateAI.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using UnityEngine;
 
@@ -5,19 +6,30 @@ using UnityEngine;
 public class TwoStateAI : Enemy
 {
     public float speed;
-    public GameObject attackPoint;
+    public GameObject attackPointRight;
+    public GameObject attackPointLeft;
+    public GameObject attackPointRanged;
+    public GameObject groundCheck;
     public float attackRadius;
     public float attackSpeed;
-    public float attactDistance;
+    public Projectile projectile;
+    public float projectileSpeed;
+    public float jumpForce;
+    public LayerMask groundLayer;
     [Header("StatesStats")]
     public bool state = true;
     //true = close range
     //false = long range
     public float stateTimer = 20.0f;
+    public float attactDistanceClose;
+    public float attackDistanceRange;
+    public float longRangeWalkSpeedModifier;
     private float currentStateTimer;
     private Transform playerLocation;
     private Rigidbody2D rb;
     private bool isWaiting = false;
+    private bool isGrounded;
+    private float rangeAttackCooldown = 0f;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -26,6 +38,7 @@ public class TwoStateAI : Enemy
     }
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, 0.1f, groundLayer);
         currentStateTimer -= Time.deltaTime;
         if (currentStateTimer <= 0)
         {
@@ -50,11 +63,27 @@ public class TwoStateAI : Enemy
     {
         if (state)
         {
+            if(playerLocation.position.x > transform.position.x) //player is to the right, jump left
+            {
+                Jump(-1);
+            }
+            else
+            {
+                Jump(1);
+            }
             state = false;
             Debug.Log("Changed state to long range");
         }
         else
         {
+            if(playerLocation.position.x > transform.position.x)
+            {
+                Jump(1);
+            }
+            else
+            {
+                Jump(-1);
+            }
             state = true;
             Debug.Log("Changed state to close range");
         }
@@ -62,7 +91,7 @@ public class TwoStateAI : Enemy
     void CloseRangeBehaviour()
     {
         Vector2 distance = playerLocation.position - gameObject.transform.position;
-        if (Mathf.Abs(distance.x) < attactDistance)
+        if (Mathf.Abs(distance.x) < attactDistanceClose)
         {
             CloseRangeAttack();
             return;
@@ -70,13 +99,34 @@ public class TwoStateAI : Enemy
         float direction = Mathf.Sign(playerLocation.position.x - transform.position.x);
         rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocity.y);
     }
-    void LongRangeBehaviour()
+    void LongRangeBehaviour() //slower movement, prefers to stay at attackDistanceRange
     {
-
+        Vector2 distance = playerLocation.position - gameObject.transform.position;
+        float direction = Mathf.Sign(playerLocation.position.x - transform.position.x);
+        if (Mathf.Abs(distance.x) < attackDistanceRange)
+        {
+            RangedAttack();
+            rb.linearVelocity = new Vector2(-1 * direction * speed * longRangeWalkSpeedModifier, rb.linearVelocity.y);
+        } else
+        {
+            rb.linearVelocity = new Vector2(direction * speed * longRangeWalkSpeedModifier, rb.linearVelocity.y);
+        }
+        rangeAttackCooldown -= Time.deltaTime;
     }
     void CloseRangeAttack()
     {
-        Collider2D[] hits = Physics2D.OverlapCircleAll(attackPoint.transform.position, attackRadius);
+        Vector3 attackPointPosition;
+        if(playerLocation.position.x < transform.position.x)
+        {
+            attackPointPosition = attackPointLeft.transform.position;
+            Debug.Log("Close Range Attack Left!");
+        }
+        else
+        {
+            attackPointPosition = attackPointRight.transform.position;
+            Debug.Log("Close Range Attack Right!");
+        }
+        Collider2D[] hits = Physics2D.OverlapCircleAll(attackPointPosition, attackRadius);
         foreach (var hit in hits)
         {
             if (hit.TryGetComponent(out PlayerHealth playerHealth))
@@ -91,6 +141,30 @@ public class TwoStateAI : Enemy
         }
         StartCoroutine(Wait(attackSpeed));
     }
+    void RangedAttack()
+    {
+        if (rangeAttackCooldown > 0f)
+        {
+            return;
+        }
+        Projectile currentProjectile = Instantiate(projectile, attackPointRanged.transform.position, Quaternion.identity);
+        currentProjectile.IgnoreParentObject(gameObject);
+        currentProjectile.damage = damage;
+        Vector2 direction = (playerLocation.position - attackPointRanged.transform.position).normalized;
+        currentProjectile.GetComponent<Rigidbody2D>().linearVelocity = direction * projectileSpeed;
+        rangeAttackCooldown = attackSpeed;
+    }
+    void Jump(int direction) //1 = right, -1 = left
+    {
+        if (!isGrounded)
+        {
+            Debug.Log("Tried to jump while not grounded");
+            return;
+        }
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+        rb.AddForce(new Vector2(direction,jumpForce * rb.mass), ForceMode2D.Impulse);
+        Debug.Log("Jumped");
+    }
     IEnumerator Wait(float time)
     {
         isWaiting = true;
@@ -99,6 +173,7 @@ public class TwoStateAI : Enemy
     }
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(attackPoint.transform.position, attackRadius);
+        Gizmos.DrawWireSphere(attackPointRight.transform.position, attackRadius);
+        Gizmos.DrawWireSphere(attackPointLeft.transform.position, attackRadius);
     }
 }

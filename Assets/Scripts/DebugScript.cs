f486b89070f17a16de47164b9ccbc2eb1c7efa3c
Michix985 <groth06michal@gmail.com>
2025-12-25 20:27:14 +0100

    improved debugging and simple hard damage system
    Currently only clicking "I" gives you 10 hard damage

--- Assets/Scripts/DebugScript.cs
+++ Assets/Scripts/DebugScript.cs
@@ -7,13 +7,19 @@ public class DebugScript : MonoBehaviour
     public GameObject debugCanvas;
     public TextMeshProUGUI bloodText;
     private bool isDebugModeActive = false;
+    private GameObject player;
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(player == null)
+        {
+            Debug.LogWarning("Player not found");
+        }
         EnableOrDisableDebug(); //enabled on by default for now
     }
     void Update()
     {
-        if(isDebugModeActive)
+        if (isDebugModeActive)
         {
             bloodText.text = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>().currentBlood.ToString(); //bloodtext.text = current blood value
         }
@@ -33,9 +39,40 @@ public class DebugScript : MonoBehaviour
         }
         else
         {
-            Debug.Log("DEBUG MODE ENABLED");
+            Debug.Log("DEBUG MODE ENABLED. Click I for action 1, O for action 2, P for action 3");
             debugCanvas.SetActive(true);
             isDebugModeActive = true;
+        }
+    }
+    //Change these during development to test stuff
+    public void OnDebugAction1(InputAction.CallbackContext context) //Activate with: I
+    {
+        if (!isDebugModeActive) return;
+        if (context.started)
+        {
+            Debug.Log("Debug action1");
+            //put stuff here
+            player.GetComponent<PlayerHealth>().GainBlackBile(10);
+        }
+    }
+    public void OnDebugAction2(InputAction.CallbackContext context) //Activate with: O
+    {
+        if (!isDebugModeActive) return;
+        if (context.started)
+        {
+            Debug.Log("Debug action2");
+            //put stuff here
+
+        }
+    }
+    public void OnDebugAction3(InputAction.CallbackContext context) //Activate with: P
+    {
+        if (!isDebugModeActive) return;
+        if (context.started)
+        {
+            Debug.Log("Debug action3");
+            //put stuff here
+
         }
     }
 }
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using TMPro;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,18 +7,25 @@ public class PlayerHealth : MonoBehaviour
 {
     public float maxBlood = 100;
     private PlayerController playerController;
-    public float currentBlood;
-    public Slider bloodSlider;
+    public Image bloodFill;
+    public Image blackBileFill;
+    [HideInInspector] public float currentBlackBile;
+    [HideInInspector] public float currentBlood;
     void Start()
     {
         playerController = GetComponent<PlayerController>();
         currentBlood = maxBlood;
-        bloodSlider.maxValue = maxBlood;
-        bloodSlider.value = currentBlood;
+        currentBlackBile = 0;
+        blackBileFill.fillAmount = 0;
     }
     void Update()
     {
-        bloodSlider.value = currentBlood;
+        bloodFill.fillAmount = currentBlood / maxBlood;
+        if(currentBlackBile > 0)
+        {
+            blackBileFill.fillAmount = currentBlackBile / maxBlood;
+            currentBlackBile -= Time.deltaTime;
+        }
     }
     public void Die()
     {
@@ -30,7 +36,7 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentBlood -= damage;
-        bloodSlider.value = currentBlood;
+        bloodFill.fillAmount = currentBlood / maxBlood;
         if (currentBlood <= 0)
         {
             Die();
@@ -41,10 +47,16 @@ public class PlayerHealth : MonoBehaviour
     public void GainBlood(float gain) //use instead of blood += n as it is likely that gaining blood will cause additional effects in the future.
     {
         currentBlood += gain;
-        if (currentBlood > maxBlood)
+        if (currentBlood > maxBlood || currentBlackBile != 0)
         {
-            currentBlood = Mathf.Clamp(currentBlood, 0, maxBlood);
+            currentBlood = Mathf.Clamp(currentBlood, 0, maxBlood - currentBlackBile);
         }
         Debug.Log("Gained blood: " + gain);
+    }
+    //BLACK BILE status effect. Is this interesting enough?
+    public void GainBlackBile(float amount)
+    {
+        currentBlackBile += amount;
+        currentBlackBile = Mathf.Clamp(currentBlackBile, 0f, maxBlood - currentBlood);
     }
 }

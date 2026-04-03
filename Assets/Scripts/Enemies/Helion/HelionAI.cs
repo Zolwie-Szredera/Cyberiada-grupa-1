a59cdecd0b4255a60058eabb24c147c29db8e90d
Michix985 <groth06michal@gmail.com>
2026-04-03 13:25:12 +0200

    added bouncy projectile prefab

--- Assets/Scripts/Enemies/Helion/HelionAI.cs
+++ Assets/Scripts/Enemies/Helion/HelionAI.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
 
-public class HelionAI :Enemy
+[RequireComponent(typeof(HelionAttack))]
+public class HelionAI : Enemy
 {
-    
+    private HelionAttack attackScript;
+    public override void Start()
+    {
+        base.Start();
+        attackScript = GetComponent<HelionAttack>();
+    }
+    public void Update()
+    {
+        if (Vector2.Distance(attackScript.attackPoint.position, playerLocation.position) <= attackScript.attackRange && attackCooldown <= 0)
+        {
+            Vector2 direction = (playerLocation.position - attackScript.attackPoint.position).normalized;
+            attackScript.ProjectileAttack(direction);
+            attackCooldown = attackSpeed;
+        }
+    }
 }
---
+++ Assets/Scripts/Projectiles/BouncyProjectile.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BouncyProjectile : Projectile
+{
+    public override void ApplyCollisionEffect(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+        //this type doesn't do friendly fire
+
+        //this type of projectile bounces off the ground and sticky walls instead of being destroyed
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground") || other.gameObject.layer == LayerMask.NameToLayer("StickyWall"))
+        {
+            //reflect the projectile's velocity based on the collision normal
+            Vector2 normal = other.ClosestPoint(transform.position) - (Vector2)transform.position;
+            normal.Normalize();
+            Vector2 newVelocity = Vector2.Reflect(GetComponent<Rigidbody2D>().linearVelocity, normal);
+            GetComponent<Rigidbody2D>().linearVelocity = newVelocity;
+        }
+    }
+}

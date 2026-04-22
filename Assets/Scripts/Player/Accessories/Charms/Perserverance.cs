10399eef3f2645d57784c851d14c4d1a17f2db58
Michix985 <groth06michal@gmail.com>
2026-04-22 14:01:44 +0200

    added new encounters to level 1

---
+++ Assets/Scripts/Player/Accessories/Charms/Perserverance.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Perserverance", menuName ="Accessories/Perserverance")]
+public class Perserverance : Accessory
+{
+    public override void Apply(PlayerStats stats)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Remove(PlayerStats stats)
+    {
+        throw new System.NotImplementedException();
+    }
+}
